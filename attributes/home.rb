e0a08c6364d82906de3d6cf883788116b7554df6
Ben Vidulich <ben@vidulich.co.nz>
2016-12-22 12:46:48 +1300

    Add OS X support for home directory

--- attributes/home.rb
+++ attributes/home.rb
@@ -1,6 +1,12 @@
 default['kronia']['user_name'] = 'ben'
 default['kronia']['user_full_name'] = 'Ben Vidulich'
 default['kronia']['user_email'] = 'ben@vidulich.co.nz'
-default['kronia']['user_home'] = '/home/ben'
+
+case node['platform_family']
+when 'debian'
+  default['kronia']['user_home'] = '/home/ben'
+when 'mac_os_x'
+  default['kronia']['user_home'] = '/Users/ben'
+end
 
 default['kronia']['group_name'] = 'ben'
